2a06c5e1fc257f63648ece1a0b4ae15c19eb952d
EC2 Default User <ec2-user@ip-10-0-1-187.ap-northeast-1.compute.internal>
2024-11-03 08:46:04 +0000

    ec2-user

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -18,7 +18,8 @@ host = ENV['TARGET_HOST']
 
 options = Net::SSH::Config.for(host)
 
-options[:user] ||= Etc.getlogin
+options[:user] ||= 'ec2-user'
+#options[:user] ||= Etc.getlogin
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
