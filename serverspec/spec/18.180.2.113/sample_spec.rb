fe467e9df0c5e4503cf6ca072620aecc9d42bc21
EC2 Default User <ec2-user@ip-10-0-1-187.ap-northeast-1.compute.internal>
2024-11-03 07:56:53 +0000

    serverspec

--- serverspec/spec/18.180.2.113/sample_spec.rb
+++ serverspec/spec/18.180.2.113/sample_spec.rb
@@ -7,33 +7,34 @@ describe package('git') do
 end  
 
 # パッケージがインストールされているか
-%w{make gcc gcc-c++ make openssl-devel libyaml-devel}.each do |pkg|
+%w{make gcc gcc-c++ openssl-devel libyaml-devel}.each do |pkg|  # makeは重複しているため削除
   describe package(pkg) do
-  it { should be_installed }
+    it { should be_installed }
   end
-  
+end
+
 # 指定のバージョンがインストールされているか  
 # ruby 3.2.3
 describe command('/home/ec2-user/.rbenv/shims/ruby -v') do
-its(:exit_status) { should eq 0 }
-its(:stdout) { should match /3\.2\.3/ }
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /3\.2\.3/ }
 end
 
 # bundler 2.3.14
 describe command("/home/ec2-user/.rbenv/shims/bundle _2.3.14_ -v") do
-its(:stdout) { should match /2\.3\.14/ }
+  its(:stdout) { should match /2\.3\.14/ }
 end
 
 # Rails 7.1.3.2
 describe command('/home/ec2-user/.rbenv/shims/rails -v') do
-its(:exit_status) { should eq 0 }
-its(:stdout) { should match /7\.1\.3\.2/ }
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /7\.1\.3\.2/ }
 end  
 
 # Node 17.9.1
 describe command('/home/ec2-user/.nvm/versions/node/v17.9.1/bin/node -v') do
-its(:exit_status) { should eq 0 }
-its(:stdout) { should match /17\.9\.1/ }
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /17\.9\.1/ }
 end
 
 # Nginxがインストール済であること  
@@ -47,7 +48,6 @@ describe port(listen_port) do
 end
 
 # curlでHTTPアクセスして200 OKが返ってくるか確認する  
-describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
+describe command("curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w \"%{http_code}\\n\" -s") do
   its(:stdout) { should match /^200$/ }
 end 
-
