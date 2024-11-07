951a38f3ecacd7be846788ef1b72ce2c519253b9
taemimizukura <t.m.october.arinto@gmail.com>
2024-11-07 04:59:04 +0000

    serverspec

--- serverspec/spec/18.180.2.113/sample_spec.rb
+++ serverspec/spec/18.180.2.113/sample_spec.rb
@@ -15,27 +15,27 @@ end
 
 # 指定のバージョンがインストールされているか  
 # ruby 3.2.3
-describe command('ruby -v') do
+describe command('/home/ec2-user/.rbenv/shims/ruby -v') do
   its(:stdout) { should match /3\.2\.3/ }
 end
 
 # Rails 7.1.3.2
-describe command('rails -v') do
+describe command('/home/ec2-user/.rbenv/shims/rails -v') do
   its(:stdout) { should match /7\.1\.3\.2/ }
 end  
 
 # Node 17.9.1
-describe command('node -v') do
+describe command('/home/ec2-user/.nvm/versions/node/v17.9.1/bin/node -v') do
   its(:stdout) { should match /17\.9\.1/ }
 end
 
 # Yarn 1.22.19
-describe command('yarn -v') do
+describe command('/home/ec2-user/.yarn/bin/yarn -v"') do
   its(:stdout) { should match /1\.22\.19/ }
 end
 
 # Bundler 2.3.14
-describe command('bundle -v') do
+describe command("cd /home/ec2-user/raisetech-live8-sample-app && /home/ec2-user/.rbenv/shims/bundle -v") do
   its(:stdout) { should match /2\.3\.14/ }
 end
 
